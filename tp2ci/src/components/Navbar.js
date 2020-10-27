d240681d869c801b5158573110ebd51888b7bce5
diplodonovan <donovanbruniquet@gmail.com>
2020-10-27 09:34:58 +0100

    mise en forme données users

---
+++ tp2ci/src/components/Navbar.js
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <h1>Chat server monitoring</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
