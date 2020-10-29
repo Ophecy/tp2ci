10c6b0b7ec853d259602d7f595646a3142ad5753
Ophecy <arthur.de.berranger@gmail.com>
2020-10-29 11:52:21 +0100

    add(ActiveUsers): support for multiple servers

--- src/components/ActiveUsers.js
+++ src/components/ActiveUsers.js
@@ -1,47 +1,58 @@
-import React, {Component} from 'react'
-
-const URL = 'http://a2berranger.alwaysdata.net/tp2/index.php';
+import React, { Component } from "react";
 
 class ActiveUsers extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      servers: [
+        "http://a2berranger.alwaysdata.net/tp2/index.php",
+        "http://a2berranger.alwaysdata.net/tp2/index2.php",
+      ],
+    };
+  }
 
+  componentDidMount() {
+    this._fetchUsers();
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { 
-            users : []
-         }
+  _fetchUsers = () => {
+    for (const e of this.state.servers) {
+      fetch(e)
+        .then((res) => res.json())
+        .then((json) => {
+          let usersModiff = [
+            ...this.state.users,
+            json.users.filter((u) => u.currentStatus === "ACTIVE"),
+          ];
+          this.setState({
+            users: usersModiff,
+          });
+        });
+    }
+  };
+
+  render() {
+    const { users } = this.state;
+    const server = window.location.origin;
+    let nbUsers = 0;
+    for (const e of users) {
+      nbUsers += e.length;
     }
 
-    componentDidMount(){
-        this._fetchUsers();
-    }
-
-
-
-    _fetchUsers =  () => {
-        
-        fetch(URL)
-        .then( (res) => res.json())
-        .then( json => {
-            this.setState({users : json.users.filter( u => u.currentStatus ==="ACTIVE")})
-        })
-    }
-
-
-    render() { 
-        const {users} = this.state
-        const server = window.location.origin
-        return (
-            <div className="col-3 p-3">
-                <div className="card bg-white p-2">
-                    <p className="text-primary font-weight-bold text-left">Active Users</p>
-                    <h1 className="text-left">{users.length}</h1>
-                    <p className="text-secondary">On servers: </p>
-                    <p className="text-info">{server}</p>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="col-3 p-3">
+        <div className="card bg-white p-2">
+          <p className="text-primary font-weight-bold text-left">
+            Active Users
+          </p>
+          <h1 className="text-left">{nbUsers}</h1>
+          <p className="text-secondary">On servers: </p>
+          <p className="text-info">{server}</p>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default ActiveUsers;
